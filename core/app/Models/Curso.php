291f80ad92fa1d70c815df2c798d690be63ae045
matheusedivado <ms1501085@gmail.com>
2025-05-19 17:10:00 -0300

    Adiciona campos fillable resource recursos

--- core/app/Models/Curso.php
+++ core/app/Models/Curso.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Curso extends Model
 {
-    //
+    protected $fillable = [
+        'turno',
+        'nome',
+        'abreviacao',
+        'qtdModulos',
+        'modalidade',
+        'horas',
+        'horasEstagio',
+        'horasTg',
+    ];
 }
