32e4effc33d8909210ceabc27f8dc77ccec6d078
matheusedivado <ms1501085@gmail.com>
2025-05-19 17:11:01 -0300

    Adiciona timestamps na tabela

--- core/database/migrations/2025_05_19_191941_create_cursos_table.php
+++ core/database/migrations/2025_05_19_191941_create_cursos_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->unsignedInteger('horas');
             $table->unsignedInteger('horasEstagio')->nullable();
             $table->unsignedInteger('horasTg')->nullable();
+            $table->timestamps();
         });
     }
 
