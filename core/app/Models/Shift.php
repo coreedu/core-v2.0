6c0e28daa00ade0127913889013722b57cc02d1a
RAI FELIPPE MIRANDA <sopararedesed@gmail.com>
2025-09-18 17:01:48 -0300

    Create shift resource

---
+++ core/app/Models/Shift.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Shift extends Model
+{
+    protected $table = 'shift';
+
+    // Campos que podem ser preenchidos em mass-assignment
+    protected $fillable = ['name', 'description'];
+}
