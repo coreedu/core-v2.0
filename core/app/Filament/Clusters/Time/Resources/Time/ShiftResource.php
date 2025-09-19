d99297d0ec83a42303c1199057d17f18bb106f97
RAI FELIPPE MIRANDA <sopararedesed@gmail.com>
2025-09-19 17:35:15 -0300

    update icon shift resource

--- core/app/Filament/Clusters/Time/Resources/Time/ShiftResource.php
+++ core/app/Filament/Clusters/Time/Resources/Time/ShiftResource.php
@@ -19,7 +19,7 @@ class ShiftResource extends Resource
 {
     protected static ?string $model = Shift::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-clock';
+    protected static ?string $navigationIcon = 'heroicon-o-briefcase';
 
     protected static ?string $cluster = Time::class;
 
