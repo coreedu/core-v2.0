a3bed4c21791ad262f784500fd40704d9f9cfcb4
RAI FELIPPE MIRANDA <sopararedesed@gmail.com>
2025-09-19 16:29:14 -0300

    add time cluster

---
+++ core/app/Filament/Clusters/Time.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Clusters;
+
+use Filament\Clusters\Cluster;
+// use Filament\Navigation\NavigationItem;
+
+class Time extends Cluster
+{
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+
+    protected static ?string $slug = 'time';
+
+}
