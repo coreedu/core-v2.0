323263bc9e90ebd13d1c2fb646ddcf9c0ab8b765
RAI FELIPPE MIRANDA <sopararedesed@gmail.com>
2025-10-04 12:44:50 -0300

    Change time cluser icon

--- core/app/Filament/Clusters/Time.php
+++ core/app/Filament/Clusters/Time.php
@@ -6,7 +6,7 @@ use Filament\Clusters\Cluster;
 
 class Time extends Cluster
 {
-    protected static ?string $navigationIcon = 'heroicon-o-calendar-days';
+    protected static ?string $navigationIcon = 'heroicon-o-clock';
     protected static ?string $slug = 'time';
     protected static ?string $navigationGroup = 'Calendário';
     protected static ?string $navigationLabel = 'Horários';
