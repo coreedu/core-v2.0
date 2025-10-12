257c0448e1946f350f09c61efcb185ac559dfa69
Matheus Edivaldo <ms1501085@gmail.com>
2025-10-12 15:52:10 -0300

    Logo core no menu lateral

--- core/app/Providers/Filament/CentralPanelProvider.php
+++ core/app/Providers/Filament/CentralPanelProvider.php
@@ -36,6 +36,7 @@ class CentralPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->brandLogo(url('images\logo01.svg'))
             ->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
