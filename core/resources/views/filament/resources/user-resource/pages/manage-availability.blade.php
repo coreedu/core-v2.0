e329a615c71bd7d4d33c069adb5a097e234d0155
RAI FELIPPE MIRANDA <sopararedesed@gmail.com>
2025-10-14 17:41:53 -0300

    Add saving icon

--- core/resources/views/filament/resources/user-resource/pages/manage-availability.blade.php
+++ core/resources/views/filament/resources/user-resource/pages/manage-availability.blade.php
@@ -1,7 +1,7 @@
 
 <x-filament::page>
     <div class="flex justify-end">
-        <x-filament::button wire:click="submit" color="success" class="mt-4" size="sm">
+        <x-filament::button wire:click="submit" color="success" class="mt-4" size="sm" icon="heroicon-o-bookmark">
             Salvar Disponibilidade
         </x-filament::button>
     </div>
