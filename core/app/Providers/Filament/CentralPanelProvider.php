8f63d3d1779a6531f2e91457b301a74267f0e51c
RAI FELIPPE MIRANDA <sopararedesed@gmail.com>
2025-10-20 15:21:20 -0300

    Add chart

--- core/app/Providers/Filament/CentralPanelProvider.php
+++ core/app/Providers/Filament/CentralPanelProvider.php
@@ -14,7 +14,8 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets;
+// use Filament\Widgets;
+use App\Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -48,8 +49,9 @@ class CentralPanelProvider extends PanelProvider
             ->discoverClusters(in: app_path('Filament/Clusters'), for: 'App\\Filament\\Clusters')
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\AccountWidget::class,
+                // Widgets\FilamentInfoWidget::class,
+                // Widgets\InfoChart::class,
             ])
             ->middleware([
                 EncryptCookies::class,
