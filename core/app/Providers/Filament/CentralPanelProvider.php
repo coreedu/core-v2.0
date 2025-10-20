f667a88c1571a06012b58a8cce0e2c4d21513ae3
Matheus Edivaldo <ms1501085@gmail.com>
2025-10-20 15:15:50 -0300

    Remove registro e diminie logo

--- core/app/Providers/Filament/CentralPanelProvider.php
+++ core/app/Providers/Filament/CentralPanelProvider.php
@@ -36,9 +36,8 @@ class CentralPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
-            ->registration()
             ->brandLogo(url('images\logo.svg'))
-            ->brandLogoHeight('2rem')
+            ->brandLogoHeight('2.5rem')
             ->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
