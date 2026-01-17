04b1a48e692e94b6e3674003ca67ca076af90454
RAI FELIPPE MIRANDA <sopararedesed@gmail.com>
2026-01-17 00:42:38 -0300

    Alter model context - add relationships

--- core/app/Models/Time/Context.php
+++ core/app/Models/Time/Context.php
@@ -3,10 +3,17 @@
 namespace App\Models\Time;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Time\TimeConfig;
 
 class Context extends Model
 {
     protected $table = 'context';
 
     protected $fillable = ['name'];
+
+    public function timeConfigs(): HasMany
+    {
+        return $this->hasMany(TimeConfig::class, 'context_id');
+    }
 }
