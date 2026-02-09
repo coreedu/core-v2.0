ceacc001dbe402c6a8b408077e8cbf2d9209a1f1
RAI FELIPPE MIRANDA <sopararedesed@gmail.com>
2026-02-09 02:08:28 -0300

    change log

--- core/app/Filament/Resources/TimeConfigResource/Pages/CreateTimeConfig.php
+++ core/app/Filament/Resources/TimeConfigResource/Pages/CreateTimeConfig.php
@@ -9,10 +9,18 @@ use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use App\Filament\Components\HelpButton;
 
 class CreateTimeConfig extends CreateRecord
 {
     protected static string $resource = TimeConfigResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            HelpButton::make('edit-config'),
+        ];
+    }
 
     protected function handleRecordCreation(array $data): Model
     {
@@ -20,33 +28,41 @@ class CreateTimeConfig extends CreateRecord
             $contextId = $data['context_id'];
             $allTabsData = $data['slots'] ?? [];
             $mainRecord = null;
+            
+            activity()->withoutLogs(function () use ($contextId, $allTabsData, &$mainRecord) {
+                foreach ($allTabsData as $shiftId => $days) {
+                    
+                    $config = TimeConfig::firstOrCreate([
+                        'context_id' => $contextId,
+                        'shift_id'   => $shiftId,
+                    ]);
 
-            foreach ($allTabsData as $shiftId => $days) {
-                
-                $config = TimeConfig::firstOrCreate([
-                    'context_id' => $contextId,
-                    'shift_id'   => $shiftId,
-                ]);
+                    if (!$mainRecord) {
+                        $mainRecord = $config;
+                    }
 
-                if (!$mainRecord) {
-                    $mainRecord = $config;
-                }
+                    $config->slots()->delete();
 
-                $config->slots()->delete();
-
-                foreach ($days as $dayId => $horariosIds) {
-                    if (is_array($horariosIds)) {
-                        foreach ($horariosIds as $lessonTimeId) {
-                            TimeSlots::create([
-                                'time_config_id' => $config->id,
-                                'day_id'         => $dayId,
-                                'lesson_time_id' => $lessonTimeId,
-                            ]);
+                    foreach ($days as $dayId => $horariosIds) {
+                        if (is_array($horariosIds)) {
+                            foreach ($horariosIds as $lessonTimeId) {
+                                TimeSlots::create([
+                                    'time_config_id' => $config->id,
+                                    'day_id'         => $dayId,
+                                    'lesson_time_id' => $lessonTimeId,
+                                ]);
+                            }
                         }
                     }
                 }
-            }
 
+            });
+
+            activity()
+                ->performedOn($mainRecord)
+                ->causedBy(auth()->user())
+                ->event('created')
+                ->log("Criou a configuração de horários para o contexto {$contextId}.");
             return $mainRecord;
         });
     }
